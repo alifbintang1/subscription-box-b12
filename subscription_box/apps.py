9a0117d2c6aba179c6b9fdd69c261ca4b6d420f2
alifbintang1 <alifbintange@gmail.com>
2024-05-24 04:27:34 +0700

    Fix initial commit

--- subscription_box/apps.py
+++ subscription_box/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class SubscriptionBoxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'subscription_box'
---
+++ subscription_management/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+
+app_name = 'subscription_management'
+
+urlpatterns = [
+    
+]
