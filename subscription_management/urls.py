80177dc77fae8f1f6e11d2f1ad33ab0c9131b042
jeanxwang <fazaqinthara629@gmail.com>
2024-05-24 21:43:14 +0700

    create bases for subscription management display

--- subscription_management/urls.py
+++ subscription_management/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
+from . import views
 
 app_name = 'subscription_management'
 
 urlpatterns = [
-    
+    path('', views.subscription_list, name='subscription_list'),
+    path('<int:subscription_id>/', views.subscription_detail, name='subscription_detail'),
+    path('history/', views.subscription_history, name='subscription_history'),
 ]
